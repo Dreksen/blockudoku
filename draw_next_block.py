5ca5691f791dc32161dcede893102dda5a58b0b3
zakir <zakirovdanil55@gmail.com>
2024-05-23 19:45:09 +0300

    исправлен вывод фигуры снизу

--- draw_next_block.py
+++ draw_next_block.py
@@ -1,11 +1,14 @@
 import pygame
 def draw_next_block(block, screen, screen_width, screen_height):
     # Определить цвета для блоков
-    block_color = (0, 0, 0)  # Черный
-
+    black_color = (0, 0, 0)  # Черный
+    blue_color = (100, 100, 250)
     # Определить позицию следующего блока
     x = screen_width // 2 - 30
     y = screen_height - 120
 
     for x_offset, y_offset in block:
-        pygame.draw.rect(screen, block_color, (x + x_offset*30, y + y_offset*30, 30, 30))
+        pygame.draw.rect(screen, blue_color, (x + x_offset*30, y + y_offset*30, 30, 30))
+        pygame.draw.rect(screen, black_color, (x + x_offset*30, y + y_offset*30, 30, 30), 3)
+
+
